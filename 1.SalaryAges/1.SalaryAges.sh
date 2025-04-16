7a58ae1d2a1b99f3820e1f90cf0b72928dd746be
yo <yo@debian.localhost>
2025-04-16 17:24:30 -0400

    template

---
+++ 1.SalaryAges/1.SalaryAges.sh
@@ -0,0 +1,11 @@
+################################################################################################
+Author:0xSkyff
+Description:
+	The script requests the age of the person and if the person has
+	less than 18 years old, a message is displayed explaining that he is young
+	to work there. If he is 18 or 19 years old, his salary will be of 12000€ and this
+	result will be displayed, in the case of having between 20 and 29 years old the
+	salary will be of 15000€, between 30 and 39 of 20000€, from 40 to 49 then
+	30000€, in other case it will say that he is already retired.
+#################################################################################################
+	
